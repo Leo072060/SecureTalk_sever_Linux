dfc57ba8cb7cd57012c7aa1b14707fcedeb935ab
twh <leo072060@163.com>
2025-09-06 20:26:58 +0800

    feat(logManager): more convenient to write log

---
+++ build/CMakeFiles/SecureTalk.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SecureTalk.dir/lib/logger/src/logger.cpp.o"
+  "CMakeFiles/SecureTalk.dir/lib/logger/src/logger.cpp.o.d"
+  "CMakeFiles/SecureTalk.dir/lib/protobuf/generated_src/msg.pb.cc.o"
+  "CMakeFiles/SecureTalk.dir/lib/protobuf/generated_src/msg.pb.cc.o.d"
+  "CMakeFiles/SecureTalk.dir/lib/protobuf/generated_src/msg_header.pb.cc.o"
+  "CMakeFiles/SecureTalk.dir/lib/protobuf/generated_src/msg_header.pb.cc.o.d"
+  "CMakeFiles/SecureTalk.dir/src/databaseManager.cpp.o"
+  "CMakeFiles/SecureTalk.dir/src/databaseManager.cpp.o.d"
+  "CMakeFiles/SecureTalk.dir/src/logManager.cpp.o"
+  "CMakeFiles/SecureTalk.dir/src/logManager.cpp.o.d"
+  "CMakeFiles/SecureTalk.dir/src/main.cpp.o"
+  "CMakeFiles/SecureTalk.dir/src/main.cpp.o.d"
+  "CMakeFiles/SecureTalk.dir/src/msgHandler.cpp.o"
+  "CMakeFiles/SecureTalk.dir/src/msgHandler.cpp.o.d"
+  "CMakeFiles/SecureTalk.dir/src/networkManager.cpp.o"
+  "CMakeFiles/SecureTalk.dir/src/networkManager.cpp.o.d"
+  "SecureTalk"
+  "SecureTalk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SecureTalk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SecureTalk.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SecureTalk.
+# This may be replaced when dependencies are built.
